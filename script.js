7acab5d04eaac2cca5943a8e1f86bbe80494eb1f
kajak1 <kajetan.korzonkiewicz@gmail.com>
2019-04-08 17:30:53 +0200

    powolutku

--- script.js
+++ script.js
@@ -11,7 +11,12 @@
     }
     
     function draw(){
-        /* tutaj bedzie content */
+        let bg_day = new Image()
+        let base = new Image()
+        base.src = 'base.png'
+        bg_day.src = 'background-day.png'
+        base.onload = () => ctx.drawImage(base, 100, 100)
+        bg_day.onload = () => ctx.drawImage(bg_day, 100, 100)
     }
 
     resizeCanvas()
