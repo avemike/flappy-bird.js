654e261c7ea2562771dd0ed82fb3792f15a31f44
avemike <fajny.michal.gajek@gmail.com>
2019-05-04 12:35:01 +0200

    Added momentum

--- js/Bird.js
+++ js/Bird.js
@@ -1,12 +1,5 @@
 class Bird {
-    constructor({
-        ctx,
-        canvas,
-        sprites,
-        width,
-        x,
-        y
-    }) {
+    constructor({ ctx, canvas, sprites, width, x, y }) {
         this.params = {
             ctx,
             canvas,
@@ -14,14 +7,38 @@ class Bird {
             width,
             ratio: 12 / 17,
             x: x || 100,
-            y: y || 100
+            y: y || 100,
+            speed: 0,
+            momentum: 2
         }
-
+        
         // Controls
         document.addEventListener('keypress', (event) => {
-            if(event.key === 'w') this.params.y -= 30
+            if(event.key === 'w') this.params.momentum = -5
         })
     }
+
+    gravity() {
+        let { momentum, y } = this.params;
+
+        if(momentum < 4) {
+            this.params.momentum += 0.1
+        }
+    }
+    move() {
+        const { momentum } = this.params;
+        this.params.y += momentum*2;
+    }
+    animate() {
+        window.requestAnimationFrame(() => {
+            clear(this.params.canvas, this.params.ctx)
+            this.render()
+            this.gravity()
+            this.move()
+            this.animate()
+        })
+    }
+
     render() {
         const {
             ctx,
@@ -29,19 +46,8 @@ class Bird {
             x,
             y,
             width,
-            ratio
+            ratio,
         } = this.params
         ctx.drawImage(sprites[0], x, y, width, width * ratio)
-    }
-    gravity() {
-        this.params.y += 2
-    }
-    animate() {
-        window.requestAnimationFrame(() => {
-            clear(this.params.canvas, this.params.ctx)
-            this.render()
-            this.gravity()
-            this.animate()
-        })
     }
 }
