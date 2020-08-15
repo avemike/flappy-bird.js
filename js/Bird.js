02f5ed180985cd6b18744b18e7fc14a4886c537e
avemike <fajny.michal.gajek@gmail.com>
2020-08-15 07:34:48 +0200

    bird rotation

--- js/Bird.js
+++ js/Bird.js
@@ -5,40 +5,51 @@ class Bird {
     this.ctx = ctx;
     this.sprites = bird;
     this.width = birdProps.width;
-    this.ratio = 12 / 17;
+    this.height = birdProps.height;
     this.x = birdProps.x;
     this.y = birdProps.startingY;
     this.speed = 0;
-    this.momentum = 2;
-
+    this.momentum = 4;
+    this.angle = Math.PI / 4;
     // Controls
     document.addEventListener('keypress', (event) => {
-      if (event.key === 'w') this.momentum = -3.4;
+      if (event.key === 'w') this.momentum = -6.8;
     });
   }
 
   gravity() {
-    const { momentum } = this;
-
-    if (momentum < 5) {
-      this.momentum += 0.17;
+    if (this.momentum < 10) {
+      this.momentum += 0.34;
     }
   }
 
+  angleControl() {
+    if (Math.sign(this.momentum) === 1 && this.angle < Math.PI / 3)
+      this.angle += this.momentum / 90;
+    else if (Math.sign(this.momentum) === -1 && this.angle > -Math.PI / 4)
+      this.angle += this.momentum / 70;
+  }
+
+  // gravity related movement
   move() {
     const { momentum } = this;
-    this.y += momentum * 2;
+    this.y += momentum;
   }
 
   draw() {
     this.render();
     this.gravity();
+    this.angleControl();
     this.move();
   }
 
   render() {
-    const { sprites, x, y, width, ratio } = this;
-    ctx.drawImage(sprites, x, y, width, width * ratio);
+    const { sprites, x, y, width, height } = this;
+    ctx.save();
+    ctx.translate(x + width / 2, y + height / 2);
+    ctx.rotate(this.angle);
+    ctx.drawImage(sprites, -width / 2, -height / 2, width, height);
+    ctx.restore();
   }
 }
 export default Bird;
