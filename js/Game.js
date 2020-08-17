7a377610eb95377e8ee221fd1d4409ac21311fe2
kajak1 <kajetan.korzonkiewicz@gmail.com>
2020-08-17 16:28:56 +0200

    score mechanism includes collision

--- js/Game.js
+++ js/Game.js
@@ -1,11 +1,11 @@
 import { birdProps, pipeProps, pipe } from './constants';
 
 class Game {
-  //
   constructor({ cleaner, pipes, bases, bird }) {
     this.drawable = [cleaner, pipes, bases, bird];
     this.bird = bird;
     this.pipes = pipes.pipes;
+    this.collided = false
     this.score = 0
     this.bestScore = 0
   }
@@ -14,9 +14,13 @@ class Game {
     const middleOfPipe = this.pipes[0].offsetX + pipeProps.width / 2
     const middleOfBird = this.bird.x + birdProps.width / 2
     const distBetweenBirdAndPipe = middleOfBird - middleOfPipe
-    // console.log(middleOfBird, middleOfPipe)
-    if(distBetweenBirdAndPipe < 3 && distBetweenBirdAndPipe >= 0){
-      console.log('punkt')
+
+    // check if bird is in the middle of the closest pipe and is not colliding with it
+    if(
+      (distBetweenBirdAndPipe < 3 && distBetweenBirdAndPipe >= 0) &&
+      this.collided == false
+      ){
+      console.log('point')
     }
   }
 
@@ -25,8 +29,10 @@ class Game {
     if (
       this.bird.x + birdProps.width < this.pipes[0].offsetX ||
       this.bird.x > this.pipes[0].offsetX + pipeProps.width
-    )
+    ){
+      this.collided = false
       return;
+    }
 
     const topBird = this.bird.y;
     const bottomBird = this.bird.y + birdProps.height;
@@ -35,6 +41,7 @@ class Game {
 
     // check if bird is colliding
     if (!(topBird > topGap && bottomBird < bottomGap)) {
+      this.collided = true
       console.log('collision');
     }
   }
