91eae6be1cef6e7c2406fc3325d8852141153bf7
avemike <fajny.michal.gajek@gmail.com>
2020-08-27 18:01:31 +0200

    logic doesn't depend on players instances

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const socketio = require('socket.io')(http);
 const PipesControls = require('./PipesControls');
 const BirdControls = require('./BirdControls');
 
+const UPDATE_MILLISECONDS = 16;
+
 app.use(express.static('dist'));
 
 const autoIncrementId = (() => {
@@ -19,17 +21,23 @@ const autoIncrementId = (() => {
 
 const pipes = new PipesControls();
 
+setInterval(() => {
+  pipes.run();
+}, UPDATE_MILLISECONDS);
+
 socketio.on('connection', (socket) => {
   // user has connected
   const bird = new BirdControls(autoIncrementId());
+
+  setInterval(() => {
+    bird.gravity();
+  }, UPDATE_MILLISECONDS);
 
   socket.emit('bird', bird.data);
   socket.broadcast.emit('otherBird', bird.data);
 
   // every frame event
   socket.on('frame', () => {
-    bird.gravity();
-    pipes.run();
     socket.emit('pipes', pipes.data);
     socket.emit('bird', bird.data);
     socket.broadcast.emit('otherBird', bird.data);
