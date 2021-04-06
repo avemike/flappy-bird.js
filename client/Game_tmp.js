5fc373097e9f2316ce11e182beb86f52517a0fae
kajak1 <kajetan.korzonkiewicz@gmail.com>
2021-04-06 23:41:51 +0200

    changed props

--- client/Game_tmp.js
+++ client/Game_tmp.js
@@ -2,17 +2,21 @@
 import SCORE_PROPS from "../configs/score";
 
 class Game_tmp {
-  constructor(ctx, bird, bases, pipes, socket) {
+  constructor(ctx, bird, enemyBirds, bases, pipes, socket) {
     this.ctx = ctx;
     this.bird = bird;
     this.bases = bases;
     this.pipes = pipes;
-    this.toDraw = [bird, bases, pipes];
-    this.data = {};
+    this.enemyBirds = enemyBirds;
+    this.toDraw = [pipes, bases, bird, enemyBirds];
+    this.data = {
+      state: "running",
+    };
     this.socket = socket;
 
     socket.on("game", (data) => {
       this.data = data;
+      // setGameState(this.data.state);
     });
 
     this.setupControls();
@@ -21,9 +25,8 @@ class Game_tmp {
   renderScore() {
     this.ctx.fillStyle = "black";
     this.ctx.font = `${SCORE_PROPS.FONT_SIZE}px ${SCORE_PROPS.FONT}`;
-    // console.log(this.bird.score, this.bird.highscore);
     this.ctx.fillText(this.bird.score, SCORE_PROPS.X, SCORE_PROPS.Y);
-    this.ctx.font = `${SCORE_PROPS.FONT_SIZE - 7}px ${SCORE_PROPS.FONT}`;
+    this.ctx.font = `${SCORE_PROPS.FONT_SIZE - 10}px ${SCORE_PROPS.FONT}`;
     this.ctx.fillText(
       this.bird.highscore,
       SCORE_PROPS.X + 20,
@@ -32,7 +35,7 @@ class Game_tmp {
   }
 
   setupControls() {
-    document.addEventListener("click", () => {
+    this.ctx.canvas.addEventListener("click", () => {
       if (this.data.state === "running") {
         this.socket.emit("start game");
       }
@@ -65,9 +68,9 @@ class Game_tmp {
 
   create_tmp() {
     this.bird.manageControls(this.data.state);
-    // execute all draw animations within given objects
-    // this.changeComponents();
 
+    // this.bird.angleControl(this.data.state);
+    // execute all draw animations within given objects
     this.toDraw.forEach((object) => {
       object.draw(this.ctx);
     });
